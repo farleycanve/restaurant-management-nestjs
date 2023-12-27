2c2bee4c4a5d050669b085d737aeb29c0b122f78
khoi vu <baokhoi.vu@gmail.com>
2023-12-27 22:03:38 +0700

    config project and add restaurant module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection as MongooseConnection } from 'mongoose';
+import { mongooseToJson } from './common/mongoosePlugins/toJson';
+import { RestaurantModule } from './core/restaurant/restaurant.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        autoIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectionFactory: (connection: MongooseConnection) => {
+          // Global plugins
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          connection.plugin(require('mongoose-unique-validator'));
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          connection.plugin(require('mongoose-paginate-v2'));
+          connection.plugin(mongooseToJson);
+          connection.on('connected', async () => {
+            console.log('Database connected');
+          });
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    RestaurantModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
+import { ValidationPipe } from '@nestjs/common';
+import { configSwagger } from './configs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT') || 3000;
+  app.setGlobalPrefix(configService.get<string>('ROOT'));
+
+  app.enableCors({ origin: true, credentials: true });
+
+  app.useStaticAssets(path.join(__dirname, '..', 'public'));
+
+  /** Global pipes */
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  /** api doc */
+  configSwagger(app);
+
+  await app.listen(PORT, () => {
+    console.clear();
+    console.log(`Server started port ${PORT}`);
+  });
 }
 bootstrap();
